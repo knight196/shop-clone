81bb3601084bb9e93222faccca592fd61d418cbb
knight196 <71406824+knight196@users.noreply.github.com>
2022-12-05 11:39:27 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -118,7 +118,7 @@ app.use(express.static(path.join(__dirname, '../frontend/build')))
 app.use('/*', (req,res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
 
 
-app.use('/', (req,res) => res.send('homepage'))
+// app.use('/', (req,res) => res.send('homepage'))
 
 app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`)
